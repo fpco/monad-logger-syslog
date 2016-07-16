a8baaef7f26c623755f7c7442df7c805565dc093
Tim Dysinger <tim@dysinger.net>
2016-07-15 17:37:06 -0700

    add conditionals so this works everywhere

--- System/Log/MonadLogger/Syslog.hs
+++ System/Log/MonadLogger/Syslog.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module System.Log.MonadLogger.Syslog
@@ -13,6 +14,10 @@ import Control.Monad.Logger
 import System.Posix.Syslog
 import Data.Text (unpack)
 import System.Log.FastLogger (fromLogStr)
+#if MIN_VERSION_hsyslog(4,0,0)
+#else
+import qualified Data.ByteString.Char8 as BS8
+#endif
 
 runSyslogLoggingT :: MonadIO m => LoggingT m a -> m a
 runSyslogLoggingT = (`runLoggingT` syslogOutput)
@@ -39,10 +44,16 @@ formattedSyslogOutput :: (Loc -> LogSource -> LogLevel -> LogStr -> LogStr)
                       -> LogStr
                       -> IO ()
 formattedSyslogOutput f l s level msg =
-  withSyslog defaultConfig $ \ syslog ->
-      syslog USER
-             (levelToPriority level)
-             (fromLogStr (f l s level msg))
+#if MIN_VERSION_hsyslog(4,0,0)
+    withSyslog undefined $ \syslog ->
+        syslog USER
+            (levelToPriority level)
+            (fromLogStr $ f l s level msg)
+#else
+    syslog
+        (levelToPriority level)
+        (BS8.unpack $ fromLogStr $ f l s level msg)
+#endif
 
 levelToPriority :: LogLevel -> Priority
 levelToPriority LevelDebug = Debug
